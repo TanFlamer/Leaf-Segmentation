d04077b0e84e8aca639a2ed910dd5809ab719ae2
TanFlamer <zxtan01@outlook.com>
2022-03-29 00:06:16 +0800

    Finished first draft

--- Coursework.m
+++ Coursework.m
@@ -39,10 +39,10 @@ blueMask = (blue > 30 & blue < 100);
 
 specialMask = (green > 1.1 * red & green > 1.1 * blue);
 specialMask2 = (green > (red + blue) / 1.4);
-specialMask3 = (red + blue > green);
+specialMask3 = (red > 1.5 * blue);
 
 hueMask = (hue >= 0.2 & hue <= 0.35);
-saturationMask = (saturation >= 0.3 & saturation <= 0.6);
+saturationMask = (saturation >= 0.6);
 valueMask = (value >= 0.2 & value <= 0.7);
 
 lumaMask = (luma >= 100 & luma <= 150);
@@ -104,36 +104,18 @@ title('Red Relative Mask');
 originalMask = uint8(specialMask & specialMask2 & blueRelativeMask & redRelativeMask);
 
 figure();
-subplot(3, 2, 1);
+subplot(2, 2, 1);
 imshow(originalMask, []);
 title('Original Mask');
 
-maskedrgbImage = uint8(zeros(size(originalMask))); % Initialize
-maskedrgbImage(:,:,1) = currentImage(:,:,1) .* originalMask;
-maskedrgbImage(:,:,2) = currentImage(:,:,2) .* originalMask;
-maskedrgbImage(:,:,3) = currentImage(:,:,3) .* originalMask;
-
-subplot(3, 2, 2);
-imshow(maskedrgbImage);
-title('Masked Original Image');
-
 removedMask = bwareaopen(originalMask,400);
-subplot(3, 2, 3);
+subplot(2, 2, 2);
 imshow(removedMask, []);
 title('Removed Mask');
 
-removedrgbImage = uint8(zeros(size(removedMask))); % Initialize
-removedrgbImage(:,:,1) = currentImage(:,:,1) .* uint8(removedMask);
-removedrgbImage(:,:,2) = currentImage(:,:,2) .* uint8(removedMask);
-removedrgbImage(:,:,3) = currentImage(:,:,3) .* uint8(removedMask);
-
-subplot(3, 2, 4);
-imshow(removedrgbImage);
-title('Removed Original Image');
-
 se = strel('disk',4);
 closedMask = imclose(removedMask,se);
-subplot(3, 2, 5);
+subplot(2, 2, 3);
 imshow(closedMask, []);
 title('Closed Mask');
 
@@ -142,70 +124,69 @@ closedrgbImage(:,:,1) = currentImage(:,:,1) .* uint8(closedMask);
 closedrgbImage(:,:,2) = currentImage(:,:,2) .* uint8(closedMask);
 closedrgbImage(:,:,3) = currentImage(:,:,3) .* uint8(closedMask);
 
-subplot(3, 2, 6);
+subplot(2, 2, 4);
 imshow(closedrgbImage);
 title('Closed Original Image');
 
 leaves = rgb2gray(closedrgbImage);
-%enhanced = adapthisteq(leaves);
-%enhanced = imsharpen(leaves);
 enhanced = imsharpen(leaves,'Radius',0.5,'Amount',1.5);
-%enhanced = imadjust(leaves);
-%enhanced = histeq(leaves);
-
-figure();
 I = enhanced;
 
-subplot(3,2,1);
+figure();
+subplot(1,3,1);
+imshow(leaves);
+title('Original Image')
+
+subplot(1,3,2);
 imshow(I);
-title('Original Image');
+title('Sharpened Image');
 
 gmag = imgradient(I);
-subplot(3,2,2);
+subplot(1,3,3);
 imshow(gmag,[])
 title('Gradient Magnitude')
 
+figure();
+
 se = strel('disk',3);
 Io = imopen(I,se);
-subplot(3,2,3);
+subplot(2,2,1);
 imshow(Io)
 title('Opening')
 
 Ie = imerode(I,se);
 Iobr = imreconstruct(Ie,I);
-subplot(3,2,4);
+subplot(2,2,2);
 imshow(Iobr)
 title('Opening-by-Reconstruction')
 
 Ioc = imclose(Io,se);
-subplot(3,2,5);
+subplot(2,2,3);
 imshow(Ioc)
 title('Opening-Closing')
 
 Iobrd = imdilate(Iobr,se);
 Iobrcbr = imreconstruct(imcomplement(Iobrd),imcomplement(Iobr));
 Iobrcbr = imcomplement(Iobrcbr);
-subplot(3,2,6);
+subplot(2,2,4);
 imshow(Iobrcbr)
 title('Opening-Closing by Reconstruction')
 
 figure();
 
 fgm = imregionalmax(Iobrcbr);
-subplot(4,2,1);
+subplot(3,2,1);
 imshow(fgm)
 title('Regional Maxima of Opening-Closing by Reconstruction')
 
 I2 = labeloverlay(I,fgm);
-subplot(4,2,2);
+subplot(3,2,2);
 imshow(I2)
 title('Regional Maxima Superimposed on Original Image')
 
 se2 = strel('disk',3);
-%se2 = strel(ones(5,5));
 fgm2 = imclose(fgm,se2);
 
-%se2 = strel('disk',1);
 se2 = strel(ones(2,2));
 fgm3 = imerode(fgm2,se2);
 
@@ -213,10 +194,8 @@ fgm4 = bwareaopen(fgm3,20);
 
 if(bwconncomp(fgm4).NumObjects > 12)
     se2 = strel('disk',7);
-    %se2 = strel(ones(5,5));
     fgm2 = imclose(fgm,se2);
 
-    %se2 = strel('disk',1);
     se2 = strel(ones(2,2));
     fgm3 = imerode(fgm2,se2);
 
@@ -224,19 +203,19 @@ if(bwconncomp(fgm4).NumObjects > 12)
 end
 
 I3 = labeloverlay(I,fgm4);
-subplot(4,2,3);
+subplot(3,2,3);
 imshow(I3)
 title('Modified Regional Maxima Superimposed on Original Image')
 
 bw = imbinarize(Iobrcbr);
-subplot(4,2,4);
+subplot(3,2,4);
 imshow(bw)
 title('Thresholded Opening-Closing by Reconstruction')
 
 D = bwdist(bw);
 DL = watershed(D);
 bgm = DL == 0;
-subplot(4,2,5);
+subplot(3,2,5);
 imshow(bgm)
 title('Watershed Ridge Lines')
 
@@ -245,29 +224,32 @@ L = watershed(gmag2);
 
 labels = imdilate(L==0,ones(3,3)) + 2*bgm + 3*fgm4;
 I4 = labeloverlay(I,labels);
-subplot(4,2,6);
+subplot(3,2,6);
 imshow(I4)
 title('Markers and Object Boundaries Superimposed on Original Image')
 
+figure();
+
 Lrgb = label2rgb(L,'jet','w','shuffle');
-subplot(4,2,7);
+subplot(2,2,1);
 imshow(Lrgb)
 title('Colored Watershed Label Matrix')
 
-subplot(4,2,8);
+subplot(2,2,2);
 imshow(I)
 hold on
 himage = imshow(Lrgb);
 himage.AlphaData = 0.3;
 title('Colored Labels Superimposed Transparently on Original Image')
 
-
-figure();
-
 colourMask = (enhanced ~= 0);
 whiteMask = ~(Lrgb(:,:,1) == 255 & Lrgb(:,:,2) == 255 & Lrgb(:,:,3) == 255);
 finalMask = colourMask & whiteMask;
 finalMask = uint8(bwareaopen(finalMask,200));
+
+subplot(2,2,3);
+imshow(finalMask,[]);
+title('Final Mask');
 
 bwconncomp(finalMask).NumObjects
 
@@ -276,6 +258,7 @@ finalImage(:,:,1) = Lrgb(:,:,1) .* finalMask;
 finalImage(:,:,2) = Lrgb(:,:,2) .* finalMask;
 finalImage(:,:,3) = Lrgb(:,:,3) .* finalMask;
 
+subplot(2,2,4);
 imshow(finalImage);
 title('Final Image');
 impixelinfo;
